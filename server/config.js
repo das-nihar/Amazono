843c84b827a19cd46ce75147d5fa4e2eb728a941
Nihar Das <nihar.das@mnscorp.net>
2019-08-27 22:57:30 +0530

    database connection

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+ database: 'mongodb+srv://Amazono_Admin:Amazono_Admin@clustern95-zs5jx.mongodb.net/test?retryWrites=true&w=majority',
+ port: 3030,
+ secret: 'NiharDas123234'
+};
--- server/server.js
+++ server/server.js
@@ -2,13 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
+const config = require('./config');
 const app = express();
 
+mongoose.connect(config.database, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+       console.log('Connected to the database');
+    }
+}); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
+app.use(cors());
 
-app.listen(3030,(err) => {
-    console.log('Listening in port 3030')
-})
+app.get('/', (req,res,next) => {
+    res.json({
+        'name': 'Nihar'
+    });
+}); 
+app.listen(config.port,(err) => {
+    console.log('Listening in port '+ config.port);
+});
