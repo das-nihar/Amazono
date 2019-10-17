12c2212d35e218e9ceb9e7152a95e015fee9fcee
Nihar Das <nihar.das@mnscorp.net>
2019-10-17 09:09:47 +0530

    Created sign up api

--- server/routes/account.js
+++ server/routes/account.js
@@ -1,11 +1,38 @@
 const exp = require('express');
 const router = exp.Router();
 const jwt = require('jsonwebtoken');
-
+const config = require('../config')
 const User = require('../models/users');
 
 router.post('/signup', (req, res, next) => {
-    console.log(req.body);
+    let user = new User();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.password = req.body.password; 
+    user.picture = user.gravatar();
+    user.isSeller = req.isSeller
+
+User.findOne({email: req.body.email}, (err, existingUser) => { // findOne is a method of mongoose to find whether the details are already existing in db or not
+    if(existingUser) {
+        res.json({
+            success: false,
+            message: 'Account with that user already exist'
+        });
+    } else {
+        user.save(); // Method of mongoose to save to db
+
+        var token = jwt.sign({
+            user: user
+        }, config.secret, {
+            expiresIn: '7d'
+        });
+        res.json({
+            success: true,
+            message: 'Enjoy your token',
+            token: token
+        });
+    }
+});
 });
 
 module.exports = router;
